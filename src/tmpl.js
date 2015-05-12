db6c37588b98c721994c1540d924f31366b553a7
liuzesen <124005330@qq.com>
2015-05-12 23:27:28 +0800

    优化接结运行代码，提取出需要使用的变量

--- src/tmpl.js
+++ src/tmpl.js
@@ -5,9 +5,39 @@
 	
 	var isDefined = function(o) { return typeof o !== 'undefined'; };
 
-	var keyValue = /(?:[\w$_]*)\s*=\s*([a-zA-Z$_][0-9a-zA-Z$_]*)/g, // 匹配原生语句中等号右边的对象名称
-		bracket = /(<%=|<%)(\s*)(.*?)(\s*%>)/g, // 匹配<%= %> 或者 <% %>
-		varValue = /[a-zA-Z$_][0-9a-zA-Z$_]*/; // 匹配JS变量名
+	var keys = {
+		'break': true,
+		'delete': true, 
+		'function': true,
+		'return': true,
+		'typeof': true,  
+		'case': true,
+		'do': true, 
+		'if': true, 
+		'switch': true,
+		'var': true,  
+		'catch': true,
+		'else': true,
+		'in': true,
+		'this': true,
+		'void': true, 
+		'continue': true,
+		'false': true,
+		'instanceof': true,
+		'throw': true,
+		'while': true,  
+		'debugger': true,
+		'finally': true,
+		'new': true,
+		'true': true,
+		'with': true,  
+		'default': true,
+		'for': true,
+		'null': true,
+		'try': true
+	};
+	var keyValue = /\s*([a-zA-Z$_][0-9a-zA-Z$_]*)/g, // 匹配原生语句中等号右边的对象名称
+		bracket = /(<%=|<%)(\s*)(.*?)(\s*%>)/g; // 匹配<%= %> 或者 <% %>
 
 	var parsing = function(tmpl) {
 		var needClose = false,
@@ -17,8 +47,14 @@
 			endCode = '\nreturn out;';
 
 		var parseNativeCode = function(nativeCode) {
-			nativeCode.replace(keyValue, function(full, key) {
-				headCode += key + '=$$data.' + key + ',';
+			nativeCode.replace(/\\[\'"]/g, '')
+					  .replace(/'.*?'/g, '')
+					  .replace(/".*?"/g, '')
+					  .replace(keyValue, function(full, key) {
+				if (!keys[key]) {
+					headCode += key + '=$$data.' + key + ',';
+					keys[key] = true;
+				}
 			});
 		};
 
@@ -35,11 +71,7 @@
 			parseStaticHtml(curPos, lBracketPos);
 
 			if (lBracket.length === 3) {
-				if (!needClose) {
-					var variable = capture.match(varValue)[0];
-					headCode += variable + '=$$data.' + variable + ',';
-				}
-				mainCode += 'out+=' + capture + ';';
+				mainCode += 'out+=' + (needClose ? '' : '$$data.') + capture + ';';
 			} else if (lBracket.length === 2) {
 				if (!needClose) {
 					parseNativeCode(capture);
